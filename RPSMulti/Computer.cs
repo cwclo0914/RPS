4ec274f98cf04d9b05713ad9cfc696c5f2b4744b
CHEUK WA CHRISTOPHER LO <cheuk.wa.lo@avantcorp.com>
2017-07-10 10:51:07 +0900

    Multi

---
+++ RPSMulti/Computer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPS
+{
+    class Computer : Player
+    {
+        //Fields
+        private Random random = new Random();
+
+        //Constructor
+        public Computer()
+        {
+        }
+
+        //Methods
+        public void Shuffle()
+        {
+            int i = random.Next(0, 3);
+            Choice = i;
+        }
+        
+    }
+}
