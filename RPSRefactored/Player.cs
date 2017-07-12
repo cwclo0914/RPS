8f57765806e2fcb099e594eb58d87e535a665e71
CHEUK WA CHRISTOPHER LO <cheuk.wa.lo@avantcorp.com>
2017-07-12 12:47:55 +0900

    Static

---
+++ RPSRefactored/Player.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSMulti
+{
+    internal class Player
+    {
+        // Fields
+        private int choice; // 0:グー、1:チョキ、2:パー
+
+        // Constructor
+        public Player()
+        {
+            this.Choice = -1;
+            this.Score = 0;
+        }
+
+        // Properties
+        public int Choice // 0:グー、1:チョキ、2:パー
+        {
+            get
+            {
+                return this.choice;
+            }
+
+            set
+            {
+                if (value >= 0 && value <= 2)
+                {
+                    this.choice = value;
+                }
+                else
+                {
+                    this.choice = -1;
+                }
+            }
+        }
+
+        public int Score { get; set; } // 後期フェーズ用
+
+        // Methods
+        public void Reset()
+        {
+            this.Choice = -1;
+        }
+        public virtual void Shuffle(Random rand)
+        {
+            this.Choice = rand.Next();
+        }
+    }
+}
